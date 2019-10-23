a29ce85ab4f09307d524432f56c6ba541047d3b1
Evgeniy Kapelko <eugene.kapelko@balanceinternet.com.au>
2019-10-23 03:24:47 +0300

    Created module for lesson by magento config

---
+++ Coupon/Block/System/Config.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Learning\Coupon\Block\System;
+
+use \Magento\Backend\Block\Template\Context;
+use \Magento\Framework\App\Config\ScopeConfigInterface;
+use \Magento\Backend\Block\Template;
+use \Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class Config
+ *
+ * @package Learning\Coupon\Block\System
+ */
+class Config extends Template
+{
+    const CONFIG_PATH_COUPON_GENERAL_VALUE = 'coupon/general/value';
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    /**
+     * Config constructor.
+     *
+     * @param Context              $context
+     * @param ScopeConfigInterface $scopeConfig
+     * @param array                $data
+     */
+    public function __construct(
+        Context $context,
+        ScopeConfigInterface $scopeConfig,
+        array $data = []
+    )
+    {
+        parent::__construct(
+            $context,
+            $data
+        );
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @param string|null $storeId
+     *
+     * @return mixed
+     */
+    public function getCustomConfig(?string $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            'coupon/general/value',
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+
+}
